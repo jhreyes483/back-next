020b72c85a20681cd5cdf9a82d93f8e9c1a35dd3
ing-javier <devjavrn@gmail.com>
2026-04-23 21:14:49 -0500

    title y CO de la pagina

--- app/nosotros/page.tsx
+++ app/nosotros/page.tsx
@@ -1,13 +1,17 @@
 import Link from 'next/link'
 import React from 'react'
 import Sidebar from '@/components/Sidebar'
+import { Metadata } from 'next'
+
+export const metadata : Metadata = { // CO de la pagina
+    title : 'Meeti - Inicio'
+}
 
 export default function Home(){
     return (
         <>
             <h1>Inicio</h1>
             <Sidebar/>
-            <Link href="/nosotros">Ir a Nosotros</Link>
         </>
     )
 }
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,5 +1,7 @@
 "use client"
+
 export default function Sidebar(){
+    
     return (
         <div>Sidebar</div>
     )
