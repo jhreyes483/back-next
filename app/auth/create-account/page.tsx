9e898be986cf5d9b62c35ca09ab97f038705fc5a
ing-javier <devjavrn@gmail.com>
2026-04-26 16:06:05 -0500

    se agrega routing de botones

--- app/auth/create-account/page.tsx
+++ app/auth/create-account/page.tsx
@@ -2,6 +2,7 @@ import RegisterForm from "@/app/src/features/auth/components/RegisterForm";
 import Heading from "@/src/shared/components/typography/Heading";
 import { generatePageTitle } from "@/src/shared/components/utils/metadata";
 import { Metadata } from "next";
+import Link from "next/link";
 
 export const metadata: Metadata = {
   title: generatePageTitle("Crear cuenta")
@@ -10,8 +11,20 @@ export const metadata: Metadata = {
 export default function RegisterPage() {
   return (
     <>
-      <Heading>Crear Cuenta</Heading>
+      <Heading level={3}>Crear Cuenta</Heading>
       <RegisterForm></RegisterForm>
+      
+      <nav className="mt-20 flex justify-between">
+          <Link
+          href={'/auth/login'}
+          className="font-bold"
+          >Iniciar Sessión</Link>
+          <Link
+          href={'/auth/forgot-password'}
+          className="font-bold"
+          >Olvide mi contraseña</Link>
+      </nav>
+      
     </>
   )
 }
--- app/auth/forgot-password/page.tsx
+++ app/auth/forgot-password/page.tsx
@@ -3,6 +3,7 @@ import ForgotPasswordForm from '@/app/src/features/auth/components/ForgotPasswor
 import Heading from '@/src/shared/components/typography/Heading'
 import { generatePageTitle } from '@/src/shared/components/utils/metadata'
 import { Metadata } from 'next'
+import Link from 'next/link'
 
 export const metadata : Metadata = {
     title : generatePageTitle('Reestablecer Password')
@@ -11,8 +12,19 @@ export const metadata : Metadata = {
 export default function ForgotPasswordPage(){
     return (
         <>
-            <Heading>Recupera tu acceso a Meeti</Heading>
+            <Heading level={3}>Recupera tu acceso a Meeti</Heading>
             <ForgotPasswordForm />
+            <nav className="mt-20 flex justify-between">
+                <Link
+                href={'/auth/login'}
+                className="font-bold"
+                >Iniciar Sessión</Link>
+                <Link
+                href={'/auth/create-account'}
+                className="font-bold"
+                >Crear cuenta</Link>
+            </nav>
+        
         </>
        
     )
