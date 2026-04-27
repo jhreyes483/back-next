1b6837e953f7cd84b53405db071666973e573405
ing-javier <devjavrn@gmail.com>
2026-04-27 14:20:56 -0500

    se genera las relaciones de autenticacion usando better-auth

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,6 +1,6 @@
-import {drizzle } from 'drizzle-orm/node-postgres'
-
+import { drizzle } from 'drizzle-orm/node-postgres'
+import { relations } from './relations';
 
 export const db = drizzle(process.env.DATABASE_URL!, {
-    
+    relations
 })
